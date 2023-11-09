c848e64fa47b88b2f502f8ac800717574fcc1b84
Nate Florencio <nflorencio@showcasepro.com.br>
2023-11-09 09:42:49 -0300

    Initial Commit.

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountControler } from './controllers/create-account.controller'
+import { envSchema } from './env'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [CreateAccountControler],
+  providers: [PrismaService],
+})
+export class AppModule {}
