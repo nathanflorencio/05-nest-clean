d8ffdbbf38f138fc4a9fa081d05d4cbb27e67857
Nate Florencio <nflorencio@showcasepro.com.br>
2023-11-10 11:48:01 -0300

    Created InfraRepositories.

---
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -0,0 +1,31 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaQuestionCommentsRepository
+  implements QuestionCommentsRepository
+{
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(questionComment: QuestionComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common'
+
 import { CreateAccountControler } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountControler,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
